715c35e7f5b6b05f74604c915ebc9d1bc9ceca64
Abdullah Al Arif <129601890+abdullah-166@users.noreply.github.com>
2025-10-31 21:35:18 +0600

    Distributed Asset Initial Controller

---
+++ FetoTech/FeroTech.Web/Controllers/DistributedAssetController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FeroTech.Web.Controllers
+{
+    public class DistributedAssetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
